a068b04ddd8fe93a58d8e84d69a503dc3e85f8d1
John Wright <JohnedwinWright@gmail.com>
2015-11-16 19:36:45 -0500

    added user to vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,7 +1,10 @@
 class Vehicle < ActiveRecord::Base
   belongs_to :model
+  belongs_to :user
+
   has_many :vehicle_features
-  has_many:features, through: :vehicle_features
-  validates_presence_of :model
-                        :price
+  has_many :features, through: :vehicle_features
+  validates_presence_of :model,
+                        :price,
+                        :user
 end
